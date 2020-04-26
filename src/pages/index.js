318813b844bb048f94791424731b870d747182ab
Andy Osei <andyosei36@gmail.com>
2020-04-26 14:43:47 +0000

    index page listing posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,32 @@
 import React from "react"
+import { graphql, Link } from "gatsby"
 
-export default () => <div>Hello world!</div>
+export const query = graphql`
+  {
+    blog {
+      posts {
+        id
+        title
+        author {
+          first_name
+          last_name
+        }
+      }
+    }
+  }
+`
+
+export default ({ data }) => (
+  <div style={{ margin: "5rem auto", width: "550px" }}>
+    {data.blog.posts.map(post => (
+      <article key={post.id}>
+        <h2>
+          <Link to={`/post/${post.id}`}>{post.title}</Link>
+        </h2>
+        <p>
+          Written by {post.author.first_name} {post.author.last_name}
+        </p>
+      </article>
+    ))}
+  </div>
+)
